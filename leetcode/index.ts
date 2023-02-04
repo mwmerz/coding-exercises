fa22d74e4d2a41546dd2e3a26673f3d6f2cac260
Michael Merz <mwmerz@protonmail.com>
2023-02-03 21:48:01 -0500

    fix: leetcode export order... because... reasons.

--- leetcode/index.ts
+++ leetcode/index.ts
@@ -2,5 +2,5 @@ export { default as twoSum } from "./01-two-sum";
 export { default as addTwo } from "./02-add-two-numbers";
 export { default as joinLines } from "./03-longest-substring";
 export { default as medianOfArrays } from "./04-median-of-arrays";
-export { default as alienDictionary } from "./953-alien-dictionary";
 export { default as zigZagConversation } from "./06-zigzag-conversation";
+export { default as alienDictionary } from "./953-alien-dictionary";
